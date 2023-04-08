c04b94c25032abc51a96ffd9fc8f209dbe2fc0a8
Samiksha Singh <94427949+singhsami@users.noreply.github.com>
2023-04-08 20:57:22 +0530

    using mongo

--- app.js
+++ app.js
@@ -69,7 +69,9 @@ app.get("/contact", function(req, res){
   res.render("contact", {contactContent: contactContent});
 });
 
+const port=process.env.PORT || 3000
 
-app.listen(3000, function() {
+
+app.listen(port, function() {
   console.log("Server started on port 3000");
 });
